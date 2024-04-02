12c0f79cc70bc217f54f9b19d1aff8da2589eaac
DeviH3 <165756373+DeviH3@users.noreply.github.com>
2024-04-02 11:27:15 +0530

    Create Perceptron.py

---
+++ Perceptron.py
@@ -0,0 +1,8 @@
+#10perception
+from sklearn.datasets import load_digits
+from sklearn.linear_model import Perceptron
+X, y = load_digits(return_X_y=True)
+clf = Perceptron(tol=1e-3, random_state=0)
+clf.fit(X, y)
+Perceptron()
+clf.score(X, y)
