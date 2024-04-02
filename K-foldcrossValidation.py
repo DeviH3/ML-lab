bb8dd480d7f71baa877463e15e5889667475bcf4
DeviH3 <165756373+DeviH3@users.noreply.github.com>
2024-04-02 21:54:42 +0530

    Create K-foldcrossValidation.py

---
+++ K-foldcrossValidation.py
@@ -0,0 +1,20 @@
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+sk_folds = StratifiedKFold(n_splits=5)
+
+scores = cross_val_score(clf, X, y, cv=sk_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
+# Print train and test split of data
+for train_index, test_index in sk_folds.split(X, y):
+    print("Train indices:", train_index)
+    print("Test indices:", test_index)
